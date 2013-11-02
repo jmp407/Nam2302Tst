375955c49d3e15dd967f302a9ffae948926441be
jmp407 <jmp407@gmail.com>
2013-11-02 12:17:42 -0500

    Mod to make infinite loop.  log i WiringPi crashed after 900+ loops.

--- testHelloWorld.js
+++ testHelloWorld.js
@@ -7,15 +7,16 @@ function myConditions()
   //this will repeat every 5 seconds
 
  DhObj=am2302.read(7);
-console.log(DhObj);
+//console.log(DhObj);
  DhTxt=JSON.stringify(DhObj);
 //console.log(DhTxt);
   //you can reset counter here
-  if (i>200){clearInterval(MyCndInt)};
+//  if (i>200){clearInterval(MyCndInt)};
   i++ ;
 }
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
+  console.log(DhObj,' ',i);
   res.end(DhTxt);
-}).listen(8337, '10.0.0.4');
-console.log('Server running at http://10.0.0.4:8337/');
+}).listen(8337, '10.0.0.4', Lcallback);
+function Lcallback(){console.log('Server running at http://10.0.0.4:8337/')}
