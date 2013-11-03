1d59254632007a76835c8404cefe4ed2b47f31ee
jmp407 <jmp407@gmail.com>
2013-11-02 19:07:57 -0500

    Update testHelloWorld.js
    formatting strings

--- testHelloWorld.js
+++ testHelloWorld.js
@@ -13,8 +13,8 @@ function myConditions()
  DhObj=am2302.read(7);
  h=DhObj.h;//.toPrecision(4);
  t=32+1.8*DhObj.t;//.toPrecision(4)*9/5+32;
- txHum="The humidity is: "+ h;
- txTemp="The temperature is: " + t;
+ txHum="The humidity is: "+ h.toPrecision(4)+"\n";
+ txTemp="The temperature is: " + t.toPrecision(4)+"\n";
 //console.log(DhObj);
  DhTxt=JSON.stringify(DhObj)+" "+i+"\n";
 //console.log(DhTxt);
@@ -26,8 +26,8 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   console.log(DhObj," ",i);
   res.write(DhTxt);
-  res.write(JSON.stringify(txHum));
-  res.write(JSON.stringify(txTemp));
+  res.write(txHum);
+  res.write(txTemp);
   res.end(JSON.stringify(DhObj));
 }).listen(8337, '10.0.0.4', Lcallback);
 function Lcallback(){console.log('Server running at http://10.0.0.4:8337/')}
