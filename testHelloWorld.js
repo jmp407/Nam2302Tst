97d3db919bd693eb39169b0098765c4b631fd7be
jmp407 <jmp407@gmail.com>
2013-11-02 21:39:40 -0500

    Update testHelloWorld.js
    Averages added.

--- testHelloWorld.js
+++ testHelloWorld.js
@@ -1,10 +1,21 @@
 var http = require('http')
 var am2302 = require('am2302');
 var MyCndInt=setInterval(myConditions, 5000);
+var DhObj=am2302.read(7);
 var i=1;
-var h=50;
-var t=70;
-var DhObj=null;
+var h=DhObj.h;//.toPrecision(4);
+var t=32+1.8*DhObj.t;//.toPrecision(4)*9/5+32;
+//var h=50;
+//var t=70;
+//var DhObj=null;
+var Hum = h;
+var HumAvg1m = h;
+var HumAvg5m = h;
+var HumAvg15m = h;
+var Temp = t;
+var TempAvg1m = t;
+var TempAvg5m = t;
+var TempAvg15m = t;
 var DhTxt="humidity and temperature"
 var txHum;
 var txHumAvg1m;
@@ -14,27 +25,34 @@ var txTemp;
 var txTempAvg1m;
 var txTempAvg5m;
 var txTempAvg15m;
-var Hum;
-var HumAvg1m;
-var HumAvg5m;
-var HumAvg15m;
-var Temp;
-var TempAvg1m;
-var TempAvg5m;
-var TempAvg15m;
 function myConditions()
 {
   //this will repeat every 5 seconds
 
  DhObj=am2302.read(7);
  h=DhObj.h;//.toPrecision(4);
- 
  t=32+1.8*DhObj.t;//.toPrecision(4)*9/5+32;
+ Hum = h;
+ HumAvg1m = HumAvg1m + (h - HumAvg1m)/20;
+ HumAvg5m = HumAvg5m + (h - HumAvg5m)/100;
+ HumAvg15m = HumAvg15m + (h - HumAvg15m)/300;
+ Temp = t;
+ TempAvg1m = TempAvg1m + (t - TempAvg1m)/20;
+ TempAvg5m = TempAvg5m + (t - TempAvg5m)/100;
+ TempAvg15m = TempAvg15m + (t - TempAvg15m)/300;
  txHum="The humidity is: "+ h.toPrecision(4)+"\n";
  txTemp="The temperature is: " + t.toPrecision(4)+"\n";
 //console.log(DhObj);
  DhTxt=JSON.stringify(DhObj)+" "+i+"\n";
 //console.log(DhTxt);
+    txHumAvg1m = "One  minute humidity average: " + HumAvg1m.toPrecision(4);
+    txHumAvg5m = "Five minute humidity average: " + HumAvg5m.toPrecision(4);
+   txHumAvg15m = "Fifteen min humidity average: " + HumAvg15m.toPrecision(4);
+ 
+    txTempAvg1m = "One  minute temperature average: " + TempAvg1m.toPrecision(4);
+    txTempAvg5m = "Five minute temperature average: " + TempAvg5m.toPrecision(4);
+    txTempAvg15m ="Fifteen min temperature average: " + TempAvg15m.toPrecision(4);
+
   //you can reset counter here
 //  if (i>200){clearInterval(MyCndInt)};
   i++ ;
@@ -45,7 +63,13 @@ http.createServer(function (req, res) {
   console.log(DhObj," ",i);
   res.write(DhTxt);
   res.write(txHum);
+  res.write(txHumAvg1m);
+  res.write(txHumAvg5m);
+  res.write(txHumAvg15m);
   res.write(txTemp);
+  res.write(txTempAvg1m);
+  res.write(txTempAvg5m);
+  res.write(txTempAvg15m);
   res.end(JSON.stringify(DhObj));
 }).listen(8337, '10.0.0.4', Lcallback);
 function Lcallback(){console.log('Server running at http://10.0.0.4:8337/')}
