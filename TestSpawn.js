eca20ff4d01de7ef5f4cce1df15df74ecd4489fd
jmp407 <jmp407@gmail.com>
2013-11-06 17:58:20 -0600

    Update TestSpawn.js

--- TestSpawn.js
+++ TestSpawn.js
@@ -3,4 +3,10 @@ var spawn = require('child_process').spawn,
     grep = spawn('grep', ['ssh']);//,    Snode;
 //    SnodeJsChld = cp.spawn('sudo /opt/node/bin/node /home/coder/node_modules/am2302/test.js');
 console.log('Spawned child pid: ' + grep.pid);
-grep.stdin.end();
+grep.on('close',function (code, signal) {
+console.log('child process terminated due to receipt of signal '+ signal);
+});
+// send SIGHUP to process
+grep.kill('SIGHUP');
+
+//grep.stdin.end();
