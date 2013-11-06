e51f335d47d404aa2c83b35cd312df01e8405f73
jmp407 <jmp407@gmail.com>
2013-11-06 17:39:14 -0600

    Update TestSpawn.js

--- TestSpawn.js
+++ TestSpawn.js
@@ -1,4 +1,6 @@
 // New file for testing
-var cp= require('child_process');//,    Snode;
-    SnodeJsChld = cp.spawn('sudo /opt/node/bin/node /home/coder/node_modules/am2302/test.js');
-
+var spawn = require('child_process').spawn,
+    grep = spawn('grep', ['ssh']);//,    Snode;
+//    SnodeJsChld = cp.spawn('sudo /opt/node/bin/node /home/coder/node_modules/am2302/test.js');
+console.log('Spawned child pid: ' + grep.pid);
+grep.stdin.end();
