76ecac56b6ac108f9197864302fc357abde033c7
jmp407 <jmp407@gmail.com>
2013-11-06 18:24:44 -0600

    Update and rename TestFork to TestFork.js

--- TestFork.js
+++ TestFork.js
@@ -2,8 +2,11 @@ var cp = require('child_process');
 
 var n = cp.fork(__dirname + '/sub.js');
 
+console.log('PID ' + n.pid);
+
 n.on('message', function(m) {
   console.log('PARENT got message:', m);
+  n.kill('SIGHUP');
 });
 
 n.send({ hello: 'world' });
