516a75b5333cd0216079114ccf85424b2a0bd1c1
jmp407 <jmp407@gmail.com>
2013-11-07 17:38:53 -0600

    Update sub.js

--- sub.js
+++ sub.js
@@ -1,12 +1,12 @@
 //get ready to read the temperature and humidity
 var am2302 = require('./');
 // read the data and measure how long it takes
-console.time('am2302');
+//console.time('am2302');
 var tAndh = am2302.read(7) ;
 //console.dir(tAndh);
-console.timeEnd('am2302');
-process.on('message', function(m) {
+//console.timeEnd('am2302');
+//process.on('message', function(m) {
 //  console.log('CHILD got message:', m);
-});
+//});
 // send the temp and hum back to Mom
 process.send(tAndh);
