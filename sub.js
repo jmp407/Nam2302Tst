fd2cd06a5648656ccfa8d8e742713a0d09e6d71b
jmp407 <jmp407@gmail.com>
2013-11-06 18:17:01 -0600

    Update sub.js

--- sub.js
+++ sub.js
@@ -1,5 +1,10 @@
+var am2302 = require('./');
+console.time('am2302');
+var tAndh = am2302.read(7) ;
+console.dir(tAndh);
+console.timeEnd('am2302');
 process.on('message', function(m) {
   console.log('CHILD got message:', m);
 });
 
-process.send({ foo: 'bar' });
+process.send(tAndh);
