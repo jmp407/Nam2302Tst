59a5f94c8bb3d054d530272e03afcd61dee3d351
jmp407 <jmp407@gmail.com>
2013-11-06 18:30:35 -0600

    Update TestFork.js

--- TestFork.js
+++ TestFork.js
@@ -1,12 +1,13 @@
 var cp = require('child_process');
-
+// Fork the process to read the meter
 var n = cp.fork(__dirname + '/sub.js');
-
+// find the process and print it's ID
 console.log('PID ' + n.pid);
-
+// get the temperature and humidity via a child message
+// and then kill the process
 n.on('message', function(m) {
   console.log('PARENT got message:', m);
   n.kill('SIGHUP');
 });
-
+// somewhere along the way, send the child a message.
 n.send({ hello: 'world' });
