7fc127475dec2a4fc2199183bb504638281f1180
jmp407 <jmp407@gmail.com>
2013-11-06 19:37:41 -0600

    Update sub.js

--- sub.js
+++ sub.js
@@ -3,10 +3,10 @@ var am2302 = require('./');
 // read the data and measure how long it takes
 console.time('am2302');
 var tAndh = am2302.read(7) ;
-console.dir(tAndh);
+//console.dir(tAndh);
 console.timeEnd('am2302');
 process.on('message', function(m) {
-  console.log('CHILD got message:', m);
+//  console.log('CHILD got message:', m);
 });
 // send the temp and hum back to Mom
 process.send(tAndh);
