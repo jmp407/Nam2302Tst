f58612dea34e9e48e0db74cedc59e58444bd2d1d
jmp407 <jmp407@gmail.com>
2013-11-15 23:34:24 -0600

    Update InputTest.js

--- InputTest.js
+++ InputTest.js
@@ -2,22 +2,9 @@ var MyCndInt=setInterval(myLogTest, 10000);
 
 var readline = require('readline'),
     rl = readline.createInterface(process.stdin, process.stdout);
-//rl.setPrompt('Sp> ');
-//rl.question('Enter a new 3 digit (1 decimal) set point: ', function(answer) {
-// var SetPoint = answer
-//  console.log('The new set point is ' + SetPoint + ' dF');
-//});
-
+//
 rl.on('line', function() {
-/*  switch(line.trim()) {
-    case 'hello':
-      console.log('world!');
-      break;
-    default:
-      console.log('Say what? I might have heard `' + line.trim() + '`');
-      break;
-  }
-*/  
+//
     rl.question('Enter a new 3 digit (1 decimal) set point: ', function(answer) {
     var SetPoint = answer;
     console.log('The new set point is ' + SetPoint + ' dF');
