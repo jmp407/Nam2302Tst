fe7c734a92425f4b38754e438cab1fe5c27ce8a2
jmp407 <jmp407@gmail.com>
2013-11-17 19:36:51 -0600

    Update NetClient.js
    more changes to come.

--- NetClient.js
+++ NetClient.js
@@ -2,7 +2,8 @@ var net = require('net');
 var client = net.connect({port: 8124},
     function() { //'connect' listener
   console.log('client connected');
-  client.write('world!\r\n');
+//  client.write('world!\r\n');
+  client.write('The new SetPoint has been registered.\r\n');
 });
 client.on('data', function(data) {
   console.log(data.toString());
