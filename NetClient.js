7064bf6d9a165b8dd8301e772562d56061251cb0
jmp407 <jmp407@gmail.com>
2013-11-17 20:01:46 -0600

    Update NetClient.js
    got some bugs to clear up

--- NetClient.js
+++ NetClient.js
@@ -3,13 +3,13 @@ var client = net.connect({port: 8124},
     function() { //'connect' listener
   console.log('client connected');
 //  client.write('world!\r\n');
-  client.write('The new SetPoint has been registered.\r\n');
+//  client.write('The new SetPoint has been registered.\r\n');
 });
 client.on('data', function(data) {
   console.log(data.toString());// this appears to be data from the server.
   //  add some code here to capture the new SetPoint
   client.write('The new SetPoint has been registered.\r\n');
-  
+  // one of these keeps repeating
   // Move the next line to another function or ???
   //client.end();
 });
