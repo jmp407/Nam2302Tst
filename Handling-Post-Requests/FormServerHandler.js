518973259b1333eda20278e53c4114bae69da012
jmp407 <jmp407@gmail.com>
2013-11-22 18:45:06 -0600

    Update FormServerHandler.js
    Changing the intent to my controller.

--- Handling-Post-Requests/FormServerHandler.js
+++ Handling-Post-Requests/FormServerHandler.js
@@ -2,14 +2,24 @@ var http = require('http');
 var splitter = require('./splitter.js');
  
 var postHTML =
-  '<html><head><title>Post Example</title></head>' +
+
+  '<html><head><title>Post Set Point Example</title></head>' +
   '<body>' +
+/*
   '<form method="post">' +
   'Your Fist Name: <input name="first_name"><br>' +
   'Your Last Name: <input name="last_name"><br>' +
   '<input type="submit">' +
   '</form>' +
+*/
+//  '<form action="http://10.0.0.4:8337/" method="POST">' + Don't need the action....
+  '<form method="POST">' +
+  'Set Point: <input type="text" name="SetPoint" value=70.0><br>' +
+  'Plant on/off: <input type="text" name="PonOff" value="off"><br>' +
+  '<input type="submit" value="Send">' +
+  '</form>' +
   '</body></html>';
+
  
 http.createServer(function (req, res) {
   var body = "";
@@ -23,12 +33,13 @@ http.createServer(function (req, res) {
     {
         var hash = splitter.formValues(body);
  
-         console.log("input1 = " + hash["first_name"]);
-         console.log("input2 = " + hash["last_name"]);
+         console.log("input1 = " + hash["SetPoint"]);
+         console.log("input2 = " + hash["PonOff"]);
  
          res.writeHead(200);
-         res.write('Hello ' + hash["first_name"] + ', ' + hash["last_name"] + '!');
-         res.end();
+         res.write(postHTML);
+         res.write('The set point and relay position is ' + hash["SetPoint"] + ', ' + hash["PonOff"] + '.');
+         res.end('Thats all folks');
          return;
     }
  
